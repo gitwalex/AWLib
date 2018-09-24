ce3a92205835d437619b99505b6913f5c4712109
gerwalex <gerwalex@gmail.com>
2018-09-24 12:37:47 +0200

    Diverses WPUmsatz

---
+++ awlib/src/main/java/de/aw/awlib/database/TableColumns.java
@@ -0,0 +1,26 @@
+/*
+ * MonMa: Eine freie Android-Application fuer die Verwaltung privater Finanzen
+ *
+ * Copyright [2015] [Alexander Winkler, 2373 Dahme/Germany]
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.aw.awlib.database;
+
+public interface TableColumns {
+    String _id = "_id";
+    String column_serverurl = "serverurl";
+    String column_userID = "userID";
+    String column_connectionType = "conType";
+    String column_maindirectory = "mainDir";
+}
