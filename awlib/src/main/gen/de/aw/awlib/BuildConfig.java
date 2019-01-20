2553c24975eb32c1e882994e6d54862e49eb7932
gerwalex <gerwalex@gmail.com>
2019-01-20 13:55:57 +0100

    Refactor

---
+++ awlib/src/main/gen/de/aw/awlib/BuildConfig.java
@@ -0,0 +1,25 @@
+/*
+ * MonMa: Eine freie Android-Application fuer die Verwaltung privater Finanzen
+ *
+ * Copyright [2015] [Alexander Winkler, 2373 Dahme/Germany]
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*___Generated_by_IDEA___*/
+
+package de.aw.awlib;
+
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
+public final class BuildConfig {
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
+}
